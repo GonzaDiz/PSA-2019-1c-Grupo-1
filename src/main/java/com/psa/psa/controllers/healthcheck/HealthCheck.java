e7db40e8a8d2cc75c78f03a9b8fe96ce6b25952e
Gonzalo Diz <gonzalo.diz@despegar.com>
2019-04-04 17:22:07 -0300

    addes getters and setters for project

--- src/main/java/com/psa/psa/controllers/healthcheck/HealthCheck.java
+++ src/main/java/com/psa/psa/controllers/healthcheck/HealthCheck.java
@@ -3,12 +3,13 @@ package com.psa.psa.controllers.healthcheck;
 import com.psa.psa.model.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HealthCheck {
 
-    @RequestMapping(value = "/health-check")
+    @RequestMapping(value = "/health-check", method = RequestMethod.GET)
     @ResponseBody
     public Project healthCheck() {
         return new Project("ABM", "In Progress");
--- src/main/java/com/psa/psa/model/Project.java
+++ src/main/java/com/psa/psa/model/Project.java
@@ -9,4 +9,20 @@ public class Project {
         this.name = name;
         this.state = state;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
