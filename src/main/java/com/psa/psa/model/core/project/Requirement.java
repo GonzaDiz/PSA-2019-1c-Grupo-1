410cf456a28e9e9dd94140592258e34d9b15a777
Flavio Perez Ondarts <perezflavio94@gmail.com>
2019-06-01 13:53:28 -0300

    requirements feature

--- src/main/java/com/psa/psa/model/core/project/Requirement.java
+++ src/main/java/com/psa/psa/model/core/project/Requirement.java
@@ -1,5 +1,38 @@
 package com.psa.psa.model.core.project;
 
-public class Requirement {
+import com.psa.psa.model.core.project.Requirement;
 
+public class Requirement{
+
+    String name;
+    String description;
+    RequirementPriority priority;
+
+    public Requirement(String aName, String aDescription){
+        this.name = aName;
+        this.description = aDescription;
+        this.priority = RequirementPriority.UNDEFINED;
+    }
+
+    public Requirement(String aName, String aDescription, RequirementPriority aPriority){
+        this.name = aName;
+        this.description = aDescription;
+        this.priority = aPriority;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public RequirementPriority getPriority(){
+        return this.priority;
+    }
+
+    public void setPriority(RequirementPriority newPriority){
+        this.priority = newPriority;
+    }
 }
--- src/main/java/com/psa/psa/model/core/project/RequirementPriority.java
+++ src/main/java/com/psa/psa/model/core/project/RequirementPriority.java
@@ -1,5 +1,8 @@
 package com.psa.psa.model.core.project;
 
 public enum RequirementPriority {
-
+	HIGH,
+	MEDIUM,
+	LOW,
+	UNDEFINED
 }
