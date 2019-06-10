54a9e28fcde156c9bc829708b8c2bd282980269a
Franco Cerquetti <franco.cerquetti@despegar.com>
2019-06-10 18:09:52 -0300

    wip

--- src/main/java/com/psa/psa/controllers/api/CreateResourceRequest.java
+++ src/main/java/com/psa/psa/controllers/api/CreateResourceRequest.java
@@ -1,4 +1,59 @@
 package com.psa.psa.controllers.api;
 
 public class CreateResourceRequest {
+
+    private long cuit;
+    private String name;
+    private String seniority;
+    private String salary;
+    private Integer limWeekHours;
+    private Integer workload;
+
+    public long getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(long cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeniority() {
+        return seniority;
+    }
+
+    public void setSeniority(String seniority) {
+        this.seniority = seniority;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public Integer getLimWeekHours() {
+        return limWeekHours;
+    }
+
+    public void setLimWeekHours(Integer limWeekHours) {
+        this.limWeekHours = limWeekHours;
+    }
+
+    public Integer getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(Integer workload) {
+        this.workload = workload;
+    }
 }
--- src/main/java/com/psa/psa/dao/resources/ResourcesDAO.java
+++ src/main/java/com/psa/psa/dao/resources/ResourcesDAO.java
@@ -4,12 +4,13 @@ import com.psa.psa.model.resources.Resource;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class ResourcesDAO {
 
-    private HashMap<Long, Resource> resources = new HashMap();
+    private Map<Long, Resource> resources = new HashMap();
 
     public void save(Resource resource) {
         this.resources.put(resource.getId(), resource);
